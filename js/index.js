1f61d34b01879b0a53373ba8925248b5ae0ac5a3
leok si <leoksi.koh@gmail.com>
2016-06-02 21:36:22 +0800

    list all prices for each hotel

--- js/index.js
+++ js/index.js
@@ -24,21 +24,34 @@ $(function(){
     url:'/api1',
     success: function(data1){
       console.log (data1);
-      
+      $('.competitor1', "#abcd").append('<p> Marriott: ' + data1.abcd + '</p>')
+      $('.competitor1', "#defg").append('<p> Pan Pacific: ' + data1.defg + '</p>')
+      $('.competitor1', "#hijk").append('<p> Marina Mandarin: ' + data1.hijk + '</p>')
+      $('.competitor1', "#mnop").append('<p> Grand Hyatt: ' + data1.mnop + '</p>')
+
+
     }
   });
   $.ajax({
     type:'GET',
     url:'/api2',
     success: function(data2){
-      console.log (data2);
+      // console.log (data2);
+      $('.competitor2', "#abcd").append('<p> Marriott: ' + data2.abcd + '</p>')
+      $('.competitor2', "#defg").append('<p> Pan Pacific: ' + data2.defg + '</p>')
+      $('.competitor2', "#hijk").append('<p> Marina Mandarin: ' + data2.hijk + '</p>')
+      $('.competitor2', "#mnop").append('<p> Grand Hyatt:  ' + data2.mnop + '</p>')
     }
   });
   $.ajax({
     type:'GET',
     url:'/api3',
     success: function(data3){
-      console.log (data3);
+      // console.log (data3);
+      $('.competitor3', "#abcd").append('<p> Marriott: ' + data3.abcd + '</p>')
+      $('.competitor3', "#defg").append('<p> Pan Pacific: ' + data3.defg + '</p>')
+      $('.competitor3', "#hijk").append('<p> Marina Mandarin: ' + data3.hijk + '</p>')
+      $('.competitor3', "#mnop").append('<p>  Grand Hyatt:  ' + data3.mnop + '</p>')
     }
   });
 });
