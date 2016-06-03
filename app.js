0ac1f0db2cdd130f15e20c9ff59ae984217da74b
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-06-03 23:15:33 +0800

    Challenge complete

--- app.js
+++ app.js
@@ -11,9 +11,55 @@ app.listen(3000, function(){
 app.use(express.static('./js'))
 app.set('view engine', 'ejs');
 
-//home
-app.get('/', function(req, res){
-  res.render('index');
+var API_ROUTE = 'https://api.myjson.com/bins';
+
+app.get('/', function(req, res) {
+  var staticApi = '1c80k';
+  var priceApis = ['2tlb8', '42lok', '15ktg'];
+  var allRequests = [staticApi].concat(priceApis);
+
+  Promise
+    .all(allRequests.map(function (apiName) {
+      return new Promise(function(resolve, reject) {
+        request.get(API_ROUTE + '/' + apiName, function (err, response, body) {
+          resolve({
+            name: apiName,
+            data: JSON.parse(body)
+          });
+        });
+      })
+    }))
+    .then(function (values) {
+      console.log(values);
+      var hotels = values[0].data.hotels;
+      var priceSources = values.slice(1);
+
+      var bestPrices = {};
+      priceSources.forEach(function (priceSource) {
+        var apiName = priceSource.name;
+        var prices = priceSource.data;
+        for (var hotelName in prices) {
+          if (!bestPrices.hasOwnProperty(hotelName) ||
+            (prices[hotelName] < bestPrices[hotelName].price)) {
+            bestPrices[hotelName] = {
+              apiName: apiName,
+              price: prices[hotelName]
+            }
+          }
+        }
+      });
+
+      hotels.forEach(function (hotel) {
+        var hotelName = hotel.id;
+        if (bestPrices.hasOwnProperty(hotelName)) {
+          hotel.bestPrice = bestPrices[hotelName];
+        }
+      });
+
+      res.render('index', {
+        hotels: hotels
+      });
+    });
 });
 
 app.locals.hoteldata = require('./hoteldata.json');
@@ -25,21 +71,21 @@ app.locals.hoteldata = require('./hoteldata.json');
 //   })
 // })
 
-app.get('/api1', function(req,res,done) {
+app.get('/api1', function(req, res) {
   request.get("https://api.myjson.com/bins/2tlb8", function (err, response, body) {
     if (err) throw err
     res.json(JSON.parse(body))
   })
 })
 
-app.get('/api2', function(req,res,done) {
+app.get('/api2', function(req, res) {
   request.get("https://api.myjson.com/bins/42lok", function (err, response, body) {
     if (err) throw err
     res.json(JSON.parse(body))
   })
 })
 
-app.get('/api3', function(req,res,done) {
+app.get('/api3', function(req, res) {
   request.get("https://api.myjson.com/bins/15ktg", function (err, response, body) {
     if (err) throw err
     res.json(JSON.parse(body))
