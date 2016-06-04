ebbdca518ac146cc2f877790858876e47836ee8a
Tay Yang Shun <tay.yang.shun@gmail.com>
2016-06-04 17:56:10 +0800

    Minor refactoring

--- app.js
+++ app.js
@@ -2,10 +2,31 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+//  Series / Sequence
+//  Pros:
+//    - Ensure that you have all data upon completion of last request
+//  Cons:
+//    - Slow, have to wait for one request to return before making the next.
+//    - Can't make use of asynchronous nature of JS
+
+//  Parallel
+//  Pros:
+//    - Faster, non-blocking. No redundant idle time.
+//  Cons:
+//    - Request completion order is not guaranteed
+
+// Questions:
+//  1. Why did you write your requests in this fashion?
+//  2. What if there are more pricing sources in future?
+//  3. What if there are more hotels in future?
+//  4. What if there are more lists of hotels in future?
+//  5. What did you choose ejs as templating engine?
+//  6. What if the pricing sources return the price in differing currencies?
+
 //set our port
 var port = process.env.PORT || 3000;
 
-app.listen(3000, function (){
+app.listen(port, function (){
   console.log(`server started on ${port}`);
 })
 app.use(express.static('./js'))
@@ -14,13 +35,13 @@ app.set('view engine', 'ejs');
 const API_ROUTE = 'https://api.myjson.com/bins';
 
 app.get('/', function (req, res) {
-  const staticApi = '1c80k';
+  const hotelApis = '1c80k';
   const priceApis = ['2tlb8', '42lok', '15ktg'];
-  const allRequests = [staticApi, ...priceApis];
+  const allRequests = [hotelApis, ...priceApis];
 
   Promise
     .all(allRequests.map(function (apiName) {
-      return new Promise(function(resolve, reject) {
+      return new Promise(function (resolve, reject) {
         request.get(`${API_ROUTE}/${apiName}`, function (err, response, body) {
           resolve({
             name: apiName,
